02b413f0d10fd1663c4786ce0a7d34cb267b16dd
piyushpk2002 <piyushpk2504@gmail.com>
2024-12-04 21:50:22 +0530

    utils code setup

--- src/index.js
+++ src/index.js
@@ -6,4 +6,22 @@ dotenv.config({
     path: './env'
 })
 
-connectDB();
+//app.on(error) becomes active and it keeps on running listening for errors till the server is active.
+//should be initialized as soon as possible in the code
+
+app.on("error", (error) => {
+    console.log("ERR ", error)
+})
+
+connectDB()
+.then(() => {
+
+    //server starts running only when app.listen() is ran
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port: ${process.env.PORT}`);
+    })
+
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!!", err);
+});
